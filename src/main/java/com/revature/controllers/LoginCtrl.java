f40bd3e20329f56dbdf9632b02913f8b3ea03e28
Sam Wood <solemankirby@gmail.com>
2017-10-27 14:11:58 -0400

    Trying to get JSON to work in LoginCtrl, kinda breaking it now but pushing so I can pull other changes

--- src/main/java/com/revature/controllers/LoginCtrl.java
+++ src/main/java/com/revature/controllers/LoginCtrl.java
@@ -1,10 +1,11 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.revature.domain.BoardUser;
 import com.revature.service.AuthService;
@@ -20,8 +21,10 @@ public class LoginCtrl
 		return "html/login.html";
 	}
 	
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	public String loginUser(BoardUser bu) {
+	@RequestMapping(value = "login", method = RequestMethod.POST,
+			consumes=MediaType.APPLICATION_JSON_VALUE/*,
+			produces=MediaType.APPLICATION_JSON_VALUE*/)
+	public String loginUser(@RequestBody BoardUser bu) {
 		System.out.println(bu);
 		System.out.println(authService);
 		System.out.println(authService.login(bu));
