6a2060465f75d0a6e29fe96f62da6c5cd00ed18c
RSarmiento <raphael_kristian@hotmail.com>
2017-11-07 11:26:44 -0500

    aspects

--- src/main/java/com/revature/aspect/AutomagicAspects.java
+++ src/main/java/com/revature/aspect/AutomagicAspects.java
@@ -27,14 +27,13 @@ public class AutomagicAspects {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd z");
 		System.setProperty("current.date", dateFormat.format(new Date()));
 	}
-	
+	//Login
 	@Before("execution(* com.revature.service.AuthService.login(..))")
 	public void login(JoinPoint jp) {
 		String message = "Attempting login";
 		LOGGER.info(message);
 	}
 	
-	//After Login
 	@AfterReturning("execution(* com.revature.service.AuthService.login(..))")
 	public void goodLogin(JoinPoint jp) {
 		String message = "Good login";
@@ -44,6 +43,25 @@ public class AutomagicAspects {
 	@AfterThrowing("execution(* com.revature.service.AuthService.login(..))")
 	public void badLogin(JoinPoint jp) {
 		String message = "Bad login";
+		LOGGER.info(message);
+	}
+	
+	//logout
+	@Before("execution(* com.revature.controllers.LoginCtrl.logout*(..))")
+	public void logout(JoinPoint jp) {
+		String message = "Attempting logout";
+		LOGGER.info(message);
+	}
+	
+	@AfterReturning("execution(* com.revature.controllers.LoginCtrl.logout*(..))")
+	public void goodLogout(JoinPoint jp) {
+		String message = "Good logout";
+		LOGGER.info(message);
+	}
+	
+	@AfterThrowing("execution(* com.revature.controllers.LoginCtrl.logout*(..))")
+	public void badLogout(JoinPoint jp) {
+		String message = "Bad logout";
 		LOGGER.info(message);
 	}
 	
@@ -104,179 +122,4 @@ public class AutomagicAspects {
 		String message = "AFTER BAD DELETE AT: " + jp.getSignature();
 		LOGGER.warn(message);
 	}
-//	
-//	//CREATES
-//	//BEFORE CREATE Logs
-//	@Before("execution(* createBoard(..))")
-//	public void beforeCreateBoard(JoinPoint jp) {
-//		String message = "ATTEMPTING CREATE BOARD: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@Before("execution(* createSwimLane(..))")
-//	public void beforeCreateSwimlane(JoinPoint jp) {
-//		String message = "ATTEMPTING CREATE SWIMLANE: " + jp.getSignature();
-//		LOGGER.info(message);
-//		
-//	}
-//	
-//	@Before("execution(* createStory(..))")
-//	public void beforeCreateStory(JoinPoint jp) {
-//		String message = "ATTEMPTING CREATE STORY: " + jp.getSignature();
-//		LOGGER.info(message);
-//		
-//	}
-//	
-//	@Before("execution(* createTask(..))")
-//	public void beforeCreateTask(JoinPoint jp) {
-//		String message = "ATTEMPTING CREATE TASK: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	
-//	//AFTER RETURN CREATE LOGS
-//	@AfterReturning("execution(* createBoard(..))")
-//	public void afterCreateBoard(JoinPoint jp) {
-//		String message = "AFTER GOOD CREATE BOARD: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@AfterReturning("execution(* createSwimLane(..))")
-//	public void afterCreateSwimlane(JoinPoint jp) {
-//		String message = "AFTER GOOD CREATE SWIMLANE: " + jp.getSignature();
-//		LOGGER.info(message);
-//		
-//	}
-//	
-//	@AfterReturning("execution(* createStory(..))")
-//	public void afterCreateStory(JoinPoint jp) {
-//		String message = "AFTER GOOD CREATE STORY: " + jp.getSignature();
-//		LOGGER.info(message);
-//		
-//	}
-//	
-//	@AfterReturning("execution(* createTask(..))")
-//	public void afterCreateTask(JoinPoint jp) {
-//		String message = "AFTER GOOD CREATE TASK: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	//AFTER EXCEPTION CREATE LOGS
-//	@AfterThrowing("execution(* createBoard(..))")
-//	public void badCreateBoard(JoinPoint jp) {
-//		String message = "AFTER BAD CREATE BOARD: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* createSwimLane(..))")
-//	public void badCreateSwimlane(JoinPoint jp) {
-//		String message = "AFTER BAD CREATE SWIMLANE: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* createStory(..))")
-//	public void badCreateStory(JoinPoint jp) {
-//		String message = "AFTER BAD CREATE STORY: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* createTask(..))")
-//	public void badCreateTask(JoinPoint jp) {
-//		String message = "AFTER BAD CREATE TASK: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	//EDITS
-//	//BEFORE EDIT Logs
-//	@Before("execution(* editBoard(..)) || execution(* updateBoard(..))")
-//	public void beforeEditBoard(JoinPoint jp) {
-//		String message = "ATTEMPTING TO EDIT BOARD: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@Before("execution(* editSwimLane(..)) || execution(* updateSwimLane(..))")
-//	public void beforeEditSwimlane(JoinPoint jp) {
-//		String message = "ATTEMPTING TO EDIT SWIMLANE: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@Before("execution(* editStory(..)) || execution(* updateStory(..))")
-//	public void beforeEditStory(JoinPoint jp) {
-//		String message = "ATTEMPTING TO EDIT STORY: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@Before("execution(* editTask(..)) || execution(* updateTask(..))")
-//	public void beforeEditTask(JoinPoint jp) {
-//		String message = "ATTEMPTING TO EDIT TASK: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	
-//	//AFTER RETURN EDIT LOGS
-//	@AfterReturning("execution(* editBoard(..)) || execution(* updateBoard(..))")
-//	public void afterEditBoard(JoinPoint jp) {
-//		String message = "AFTER GOOD EDIT BOARD: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@AfterReturning("execution(* editSwimLane(..)) || execution(* updateSwimLane(..))")
-//	public void afterEditSwimlane(JoinPoint jp) {
-//		String message = "AFTER GOOD EDIT SWIMLANE: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@AfterReturning("execution(* editStory(..)) || execution(* updateStory(..))")
-//	public void afterEditStory(JoinPoint jp) {
-//		String message = "AFTER GOOD EDIT STORY: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	@AfterReturning("execution(* editTask(..)) || execution(* updateTask(..))")
-//	public void afterEditTask(JoinPoint jp) {
-//		String message = "AFTER GOOD EDIT TASK: " + jp.getSignature();
-//		LOGGER.info(message);
-//	}
-//	
-//	//AFTER EXCEPTION CREATE LOGS
-//	@AfterThrowing("execution(* editBoard(..)) || execution(* updateBoard(..))")
-//	public void badEditBoard(JoinPoint jp) {
-//		String message = "AFTER BAD EDIT BOARD: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* editSwimLane(..)) || execution(* updateSwimLane(..))")
-//	public void badEditSwimlane(JoinPoint jp) {
-//		String message = "AFTER BAD EDIT SWIMLANE: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* editStory(..)) || execution(* updateStory(..))")
-//	public void badEditStory(JoinPoint jp) {
-//		String message = "AFTER BAD EDIT STORY: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-//	
-//	@AfterThrowing("execution(* editTask(..)) || execution(* updateTask(..))")
-//	public void badEditTask(JoinPoint jp) {
-//		String message = "AFTER BAD EDIT TASK: " + jp.getSignature();
-//		LOGGER.warn(message);
-//	}
-
-	
 }
